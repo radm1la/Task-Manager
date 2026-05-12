a60985e501894bc5602380eea1987f96d78206d8
Radmi1a05 <128687368+radm1la@users.noreply.github.com>
2026-05-12 14:02:47 +0400

    add:header component,taks model

---
+++ src/app/core/services/task.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Task } from '../../shared/models/task.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TaskService {
+
+
+}
