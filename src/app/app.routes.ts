a22549595c241e4eb85f1bcc540ecd45096cd3b9
Radmi1a05 <128687368+radm1la@users.noreply.github.com>
2026-05-12 14:53:08 +0400

    add:sidebar & configure routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { Board } from './features/board/board';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',redirectTo:'board',pathMatch:'full'},
+    {path:'board',component:Board}
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./layout/header/header";
+import { Sidebar } from "./layout/sidebar/sidebar";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, Sidebar],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
